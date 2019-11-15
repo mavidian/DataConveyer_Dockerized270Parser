84c265a9cb8d198fa45f45f9a1e66bb50bd0e49d
Wojtek Dziegielewski <wojtekd@mavidian.com>
2019-11-15 00:04:30 -0500

    Initial WebAPI solution.

---
+++ DataConveyer_Dockerized270Parser/Controllers/Parse270Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConveyer_Dockerized270Parser.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class Parse270Controller : ControllerBase
+   {
+      public Parse270Controller(ILogger<Parse270Controller> logger)
+      {
+      }
+
+      [HttpGet]
+      [ProducesResponseType(StatusCodes.Status400BadRequest)]
+      public IActionResult Get()
+      {
+         return BadRequest("Nothing to GET here. Use http POST method passing EDI 270 transaction in the request body.");
+      }
+
+      [HttpPost]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      [ProducesResponseType(StatusCodes.Status400BadRequest)]
+      public async Task<ActionResult<string>> Post()
+      {
+         using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+         {
+            var data = await reader.ReadToEndAsync();
+            if (data == "xyz") return BadRequest();
+            return Ok("Data Sent: " + data);
+         }
+      }
+   }
+}
